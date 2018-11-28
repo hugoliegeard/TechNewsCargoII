3c49f99977d7fe85300ad07609b77784f382122a
Hugo LIEGEARD <wf3@hl-media.fr>
2018-11-28 17:17:27 +0100

    Travail du 28/11
    Installation de Symfony
    Découverte Controller / Routing / Twig
    Mise en Place du Thème TechNews : Accueil, Catégorie et Article.

---
+++ src/Controller/TechNews/FrontController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller\TechNews;
+
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends Controller
+{
+    /**
+     * Page d'accueil de notre site internet
+     */
+    public function index()
+    {
+        # return new Response("<html><body><h1>PAGE D'ACCUEIL</h1></body></html>");
+        return $this->render('front/index.html.twig');
+    }
+
+    /**
+     * Page permettant d'afficher les articles
+     * d'une catégorie.
+     * @Route("/categorie/{slug<[a-zA-Z1-9\-_\/]+>}",
+     *     name="index_categorie",
+     *     defaults={"slug":"breaking-news"},
+     *     methods={"GET"},
+     *     requirements={"slug":"\w+"})
+     * @param $slug
+     * @return Response
+     */
+    public function categorie($slug)
+    {
+        # return new Response("<html><body><h1>PAGE CATEGORIE : $slug</h1></body></html>");
+        return $this->render('front/categorie.html.twig');
+    }
+
+    /**
+     * Afficher un Article
+     * @Route("/{categorie<\w+>}/{slug<[a-zA-Z1-9\-_\/]+>}_{id<\d+>}.html",
+     *     name="index_article")
+     * @param $id
+     * @param $slug
+     * @param $categorie
+     * @return Response
+     */
+    public function article($id, $slug, $categorie)
+    {
+        # Exemple d'URL
+        # politique/les-gilets-jaunes-mettent-le-feu-a-l-elysee_684651.html
+
+        return $this->render('front/article.html.twig');
+    }
+}
